c716ec0be7199719cb2e613652b36b6034202b14
AndresRios <andresriosbolivar27@gmail.com>
2019-09-17 12:07:20 -0500

    PropertiesPage Fix Title

--- MyLeasing.Prism/MyLeasing.Prism/ViewModels/LoginPageViewModel.cs
+++ MyLeasing.Prism/MyLeasing.Prism/ViewModels/LoginPageViewModel.cs
@@ -2,73 +2,34 @@
 using MyLeasing.Common.Services;
 using Prism.Commands;
 using Prism.Navigation;
-using System;
 
 namespace MyLeasing.Prism.ViewModels
 {
     public class LoginPageViewModel : ViewModelBase
     {
+        private readonly INavigationService _navigationService;
+        private readonly IApiService _apiService;
         private string _password;
         private bool _isRunning;
         private bool _isEnabled;
         private DelegateCommand _loginCommand;
-        private readonly IApiService _apiService;
 
-        public LoginPageViewModel(INavigationService navigationService,
+        public LoginPageViewModel(
+            INavigationService navigationService,
             IApiService apiService) : base(navigationService)
         {
+            _navigationService = navigationService;
+            _apiService = apiService;
             Title = "Login";
             IsEnabled = true;
-            _apiService = apiService;
 
-            //TODO: Delete this lines
-
+            //TODO: delete this lines
             Email = "jzuluaga55@hotmail.com";
             Password = "123456";
         }
 
         public DelegateCommand LoginCommand => _loginCommand ?? (_loginCommand = new DelegateCommand(Login));
-        private async void Login()
-        {
-            if (string.IsNullOrEmpty(Email))
-            {
-                await App.Current.MainPage.DisplayAlert("Error", "You must enter an email.", "Accept");
-                return;
-            }
-            if (string.IsNullOrEmpty(Password))
-            {
-                await App.Current.MainPage.DisplayAlert("Error", "You must enter an password.", "Accept");
-                return;
-            }
-            IsRunning = true;
-            IsEnabled = false;
 
-            var request = new TokenRequest
-            {
-                Password = Password,
-                Username = Email
-            };
-
-            var url = App.Current.Resources["UrlAPI"].ToString();
-            var response = await _apiService.GetTokenAsync(url, "Account", "/CreateToken", request);
-
-            IsEnabled = false;
-            IsRunning = true;
-
-            if (!response.IsSuccess)
-            {
-                IsEnabled = true;
-                IsRunning = false;
-                await App.Current.MainPage.DisplayAlert("Error", "User or password incorrect.", "Accept");
-                Password = string.Empty;
-                return;
-            }            
-            await App.Current.MainPage.DisplayAlert("Confirmation", "FUCKKKKK", "Accept");
-            IsEnabled = true;
-            IsRunning = false;
-        }
-
-        //Propiedad que no cambio en la view
         public string Email { get; set; }
 
         public string Password
@@ -87,6 +48,71 @@ namespace MyLeasing.Prism.ViewModels
         {
             get => _isEnabled;
             set => SetProperty(ref _isEnabled, value);
+        }
+
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(Email))
+            {
+                await App.Current.MainPage.DisplayAlert("Error", "You must enter an email.", "Accept");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(Password))
+            {
+                await App.Current.MainPage.DisplayAlert("Error", "You must enter a password.", "Accept");
+                return;
+            }
+
+            IsRunning = true;
+            IsEnabled = false;
+
+            var url = App.Current.Resources["UrlAPI"].ToString();
+            var connection = await _apiService.CheckConnectionAsync(url);
+            if (!connection)
+            {
+                IsEnabled = true;
+                IsRunning = false;
+                await App.Current.MainPage.DisplayAlert("Error", "Check the internet connection.", "Accept");
+                return;
+            }
+
+            var request = new TokenRequest
+            {
+                Password = Password,
+                Username = Email
+            };
+
+            var response = await _apiService.GetTokenAsync(url, "Account", "/CreateToken", request);
+
+            if (!response.IsSuccess)
+            {
+                IsRunning = false;
+                IsEnabled = true;
+                await App.Current.MainPage.DisplayAlert("Error", "User or password incorrect.", "Accept");
+                Password = string.Empty;
+                return;
+            }
+
+            var token = response.Result;
+            var response2 = await _apiService.GetOwnerByEmailAsync(url, "api", "/Owners/GetOwnerByEmail", "bearer", token.Token, Email);
+            if (!response2.IsSuccess)
+            {
+                IsRunning = false;
+                IsEnabled = true;
+                await App.Current.MainPage.DisplayAlert("Error", "Problem with user data, call support.", "Accept");
+                return;
+            }
+
+            var owner = response2.Result;
+            var parameters = new NavigationParameters
+            {
+                { "owner", owner }
+            };
+
+            await _navigationService.NavigateAsync("PropertiesPage", parameters);
+            IsRunning = false;
+            IsEnabled = true;
         }
     }
 }
