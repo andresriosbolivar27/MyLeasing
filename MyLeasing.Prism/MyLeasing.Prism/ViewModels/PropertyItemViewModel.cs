054cae1991a7100f3cb0560a6af32350640e5b01
AndresRios <andresriosbolivar27@gmail.com>
2019-09-17 19:01:56 -0500

    Show Property Detail Mobile

--- MyLeasing.Prism/MyLeasing.Prism/ViewModels/PropertyItemViewModel.cs
+++ MyLeasing.Prism/MyLeasing.Prism/ViewModels/PropertyItemViewModel.cs
@@ -1,20 +1,16 @@
-﻿using MyLeasing.Common.Helpers;
-using MyLeasing.Common.Models;
-using Newtonsoft.Json;
+﻿using MyLeasing.Common.Models;
 using Prism.Commands;
 using Prism.Navigation;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
-namespace MyLeasing.Prism.ViewModels
+namespace MyLeasing.Prism.ViewsModels
 {
     public class PropertyItemViewModel : PropertyResponse
     {
         private readonly INavigationService _navigationService;
         private DelegateCommand _selectPropertyCommand;
 
-        public PropertyItemViewModel(INavigationService navigationService)
+        public PropertyItemViewModel(
+            INavigationService navigationService)
         {
             _navigationService = navigationService;
         }
@@ -23,8 +19,12 @@ namespace MyLeasing.Prism.ViewModels
 
         private async void SelectProperty()
         {
-            Settings.Property = JsonConvert.SerializeObject(this);
-            await _navigationService.NavigateAsync("PropertyTabbedPage");
+            var parameters = new NavigationParameters
+            {
+                { "property", this}
+            };
+
+            await _navigationService.NavigateAsync("PropertyPage", parameters);
         }
     }
 }
